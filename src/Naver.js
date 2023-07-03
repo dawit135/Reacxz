f47a59091fa394c61af898a740bef8b8cfea866c
sdfv <dawittekie@Dawits-MacBook-Pro.local>
2023-07-03 17:46:56 +0300

    updated project

--- src/Naver.js
+++ src/Naver.js
@@ -6,7 +6,7 @@ import './App.css';
 
 const Naver = ()=>{
 	return(
-           <div>
+           <div className="navigation">
              <div>
                 <Link to="/home" className="homeLink">Home</Link>
              </div>
