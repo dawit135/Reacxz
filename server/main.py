3f985260e7c0b55eecffb4ed1ad2059b358e59c6
dawit135 <davotekie@gmail.com>
2023-07-04 20:03:49 +0300

    updated file

---
+++ server/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.params import Body,Depends
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post('/register')
+def reg(stuff=Body(...)):
+	print(stuff)
+	return {"advadsv":"advasdvas"}
+
+
+@app.post('/login')
+def log(stuff=Body(...)):
+	print(stuff)
+	return {"sdvadsv":"adadsvasdv"}
+
+
+
+
+
+
+if __name__ == "__main__":
+	uvicorn.run("main:app",host="0.0.0.0", port=9000, reload=True)
--- src/Files/API.js
+++ src/Files/API.js
@@ -1,5 +1,18 @@
+//import {useEffect} from 'react';
+
+
+
 export const register = async(newUser)=>{
 	   console.log(newUser)
+	  
+
+            fetch("http://localhost:9000/register",{
+                   method:'POST',
+                   headers:{
+                   	  'content-type':'application/json'
+                   },
+                   body:JSON.stringify(newUser)
+            })
 
 }
 
